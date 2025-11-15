1f27a31de8445a2f606eedc96c87ef83895d9ace
Arjan-Chaudhary <arjan25csu385@ncuindia.edu>
2025-11-15 23:02:19 +0530

    Update Q 22.c

--- Assignment 3.c/Q 22.c
+++ Assignment 3.c/Q 22.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[10001];
+    scanf("%s", word);
+
+    int x = 0, y = 0 ;
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (word[i] == 'z' || word[i] == 'Z')
+            x++;
+        else if (word[i] == 'o' || word[i] == 'O')
+            y++;
+    }
+    if (2 * x == y)
+        printf("Yes");
+    else
+        printf("No");
+
+    return 0;
+}
 
